9b4066a3db68b4b93647326cce1cb967932ae8a1
recepeneskaya <rkaya21@posta.pau.edu.tr>
2024-07-02 11:51:47 +0300

    feat: add ServiceView,BlogView,ContactView

--- corporate/tcore/views.py
+++ corporate/tcore/views.py
@@ -7,3 +7,15 @@ class IndexView(TemplateView):
 
 class AboutView(TemplateView):
     template_name = 'about.html'
+
+
+class ServiceView(TemplateView):
+    template_name = 'services.html'
+
+
+class BlogView(TemplateView):
+    template_name = 'blog.html'
+
+
+class ContactView(TemplateView):
+    template_name = 'contact.html'
