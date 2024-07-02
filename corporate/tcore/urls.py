cb9f3e71dc37a29618becf647d6700ca85f5069e
recepeneskaya <rkaya21@posta.pau.edu.tr>
2024-07-02 11:13:51 +0300

    feat: add path for about.html

--- corporate/tcore/urls.py
+++ corporate/tcore/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView,AboutView
 
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('about', AboutView.as_view(), name='about')
 ]
