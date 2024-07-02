d283cbbbc84e36680479d6a63a41347c7117e368
recepeneskaya <rkaya21@posta.pau.edu.tr>
2024-07-02 11:51:14 +0300

    feat: add path services,blog and contant.

--- corporate/tcore/urls.py
+++ corporate/tcore/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import IndexView,AboutView
+from .views import IndexView,AboutView,ServiceView,BlogView,ContactView
 
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('about', AboutView.as_view(), name='about')
+    path('about', AboutView.as_view(), name='about'),
+    path('services', ServiceView.as_view(), name='services'),
+    path('blog', BlogView.as_view(), name='blog'),
+    path('contact',ContactView.as_view(), name='contact')
 ]
