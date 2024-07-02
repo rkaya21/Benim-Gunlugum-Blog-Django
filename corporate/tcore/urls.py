517c030b88239ea8de1d739497f69ec5477813c3
recepeneskaya <rkaya21@posta.pau.edu.tr>
2024-07-02 10:16:33 +0300

    feat: Add IndexView for index.html

--- corporate/tcore/urls.py
+++ corporate/tcore/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import IndexView, About
+from .views import IndexView
 
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('about', About.as_view(), name='about'),
 ]
