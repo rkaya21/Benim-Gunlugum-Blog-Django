f5178b87123061607f0cb9433be61fef5960c0ba
recepeneskaya <rkaya21@posta.pau.edu.tr>
2024-07-03 17:06:12 +0300

    feat: contact about service created in database

--- corporate/tcore/models.py
+++ corporate/tcore/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from ckeditor.fields import RichTextField
+from django.utils.text import slugify
 
-# Create your models here.
+class Contact(models.Model):
+    full_name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField()
+    message = models.TextField()
+
+
+class About(models.Model):
+    title = models.CharField(max_length=200)
+    content = RichTextField()
+
+
+class Service(models.Model):
+    title = models.CharField(max_length=200)
+    content = RichTextField()
+    slug = models.SlugField(max_length=200, blank=True, editable=False)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Service, self).save(*args, **kwargs)
+
+
+
+
