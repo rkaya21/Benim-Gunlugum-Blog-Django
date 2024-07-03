48f7459dad0765aba07b1e2fdf412b5bdc84b7ca
recepeneskaya <rkaya21@posta.pau.edu.tr>
2024-07-03 17:04:53 +0300

    feat: create Media py push modules in class

---
+++ corporate/tcore/admin_mixins.py
@@ -0,0 +1,10 @@
+class CommonMedia:
+    class Media:
+        js = (
+            'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js',
+            'https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js',
+            'modeltranslation/js/tabbed_translation_field.js',
+        )
+        css = {
+            'all': ('modeltranslation/css/tabbed_translation_field.css',)
+        }
