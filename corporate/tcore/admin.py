a0cc92d5e55c8fde53e32f6a6802f3e3811aed01
recepeneskaya <rkaya21@posta.pau.edu.tr>
2024-07-03 17:02:51 +0300

    feat: save Contact Module, About Module, Service Module

--- corporate/tcore/admin.py
+++ corporate/tcore/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Contact, About, Service
+from modeltranslation.admin import TranslationAdmin
+from .admin_mixins import CommonMedia
 
-# Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'phone', 'email', 'message')
+
+
+@admin.register(About)
+class AboutAdmin(TranslationAdmin, CommonMedia):
+    list_display = ('title',)
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(Service)
+class ServiceAdmin(TranslationAdmin):
+    last_display = ('title',)
