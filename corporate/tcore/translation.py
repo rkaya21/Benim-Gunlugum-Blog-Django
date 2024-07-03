5f6ec0d4821b0383ec93080b72478c2de8575cc2
recepeneskaya <rkaya21@posta.pau.edu.tr>
2024-07-03 17:06:54 +0300

    feat: create translation py

---
+++ corporate/tcore/translation.py
@@ -0,0 +1,11 @@
+from modeltranslation.translator import register, TranslationOptions
+from .models import About, Service
+
+
+@register(About)
+class AboutTranslationOptions(TranslationOptions):
+    fields = ('title', 'content')
+
+@register(Service)
+class ServiceTranslationOptions(TranslationOptions):
+    fields = ('title',)
