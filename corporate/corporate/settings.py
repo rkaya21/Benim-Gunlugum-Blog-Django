4e86d34430929cf69874bc76e8efc8ab6e4cd91f
recepeneskaya <rkaya21@posta.pau.edu.tr>
2024-07-04 11:29:16 +0300

    feat: add category and blog class .

--- corporate/corporate/settings.py
+++ corporate/corporate/settings.py
@@ -130,7 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (BASE_DIR / 'static', )
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
