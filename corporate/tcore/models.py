5ee03fbf9d1d79aba4e19398d6338023ee99f08a
recepeneskaya <rkaya21@posta.pau.edu.tr>
2024-07-08 16:47:02 +0300

    feat: add comment line

--- corporate/tcore/models.py
+++ corporate/tcore/models.py
@@ -5,6 +5,9 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Contact(models.Model):
+    """
+    İletişim için veri tabanım
+    """
     full_name = models.CharField(max_length=100, verbose_name=_("Ad Soyad"))
     phone = models.CharField(max_length=15, verbose_name=_("Telefon Numarası"))
     email = models.EmailField()
@@ -12,17 +15,24 @@ class Contact(models.Model):
 
 
 class About(models.Model):
+    """
+    Hakkımda için veri tabanı
+    """
     title = models.CharField(max_length=200, verbose_name=_("Başlık"))
     content = RichTextField(verbose_name=_("İçerik"))
 
 
 class Service(models.Model):
+    """
+    Servisler için veri tabanı
+    """
     title = models.CharField(max_length=200, verbose_name=_("Başlık"))
     content = RichTextField(verbose_name=_("İçerik"))
     slug = models.SlugField(max_length=200, blank=True, editable=False)
 
     def save(self, *args, **kwargs):
         if not self.slug:
+            #  Başlık'a göre slug oluştur
             self.slug = slugify(self.title)
         super(Service, self).save(*args, **kwargs)
 
@@ -31,16 +41,23 @@ class Service(models.Model):
 
 
 class Slider(models.Model):
+    """
+    Slider için veri tabanı
+    """
     title = models.CharField(max_length=200, verbose_name=_("Başlık"))
     image = models.ImageField(upload_to='sliders', verbose_name=_("Resim"))
 
 
 class Category(models.Model):
+    """
+    Kategori için veri tabanı
+    """
     name = models.CharField(max_length=200, verbose_name=_("İsim"))
     slug = models.SlugField(max_length=100, unique=True, blank=True, editable=False)
 
     def save(self, *args, **kwargs):
         if not self.slug:
+            #  Kategorinin ismine göre slug oluşturur
             self.slug = slugify(self.name)
         super(Category, self).save(*args, **kwargs)
 
@@ -49,6 +66,9 @@ class Category(models.Model):
 
 
 class Blog(models.Model):
+    """
+    Blog için veri tabanı
+    """
     title = models.CharField(max_length=200, verbose_name=_("Başlık"))
     image = models.ImageField(upload_to='blogs', verbose_name=_("Resim"))
     content = RichTextField(verbose_name=_("İçerik"))
@@ -60,6 +80,7 @@ class Blog(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.slug:
+            #  Bloğun ismine göre slug oluşturur.
             self.slug = slugify(self.title)
         super(Blog, self).save(*args, **kwargs)
 
@@ -68,6 +89,9 @@ class Blog(models.Model):
 
 
 class Setting(models.Model):
+    """
+    Ayarlar için veri tabanı
+    """
     logo_1 = models.ImageField(upload_to='dimg', null=True, blank=True)
     logo_2 = models.ImageField(upload_to='dimg', null=True, blank=True)
     title = models.CharField(max_length=255, verbose_name=_("Başlık"))
