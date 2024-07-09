454c9633baabf61c8b450c99e9259b24f17f466a
recepeneskaya <rkaya21@posta.pau.edu.tr>
2024-07-09 09:54:32 +0300

    feat: add taggit module inside installed_apps

--- corporate/corporate/settings.py
+++ corporate/corporate/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'tcore',
     'ckeditor',
     'modeltranslation',
+    'taggit',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
